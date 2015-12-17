6026151abd9b43a51c9e01f2f303b42a95ea7faa
sdockray <sean.patrick.dockray@gmail.com>
2015-12-17 17:47:20 +1100

    sort by date

--- server.py
+++ server.py
@@ -59,10 +59,10 @@ def db_get(value, key='_id'):
 def db_get_all():
         global db
         data = {}
-        mp3s = db.mp3s.find(sort=[('date', pymongo.DESCENDING)])
+        mp3s = db.mp3s.find()
         for mp3 in mp3s:
                 data[mp3['fullpath']] = mp3
-        return data
+        return sorted(data, key=lambda k: k['date'], reverse=True) 
 
 # Inserts a record
 def db_insert(data):
